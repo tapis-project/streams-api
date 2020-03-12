3dc2ddc4ff6fc55def47b32a436b49bfd8b2d56e
scleveland <Sean Cleveland>
2020-03-12 15:49:00 -0600

    influx query measurements

--- service/influx.py
+++ service/influx.py
@@ -13,7 +13,7 @@ logger = get_logger(__name__)
 
 from influxdb import InfluxDBClient
 
-influx_client = InfluxDBClient(host=conf.influxdb_host, port=conf.influxdb_port, database=conf.influxdb_database)
+influx_client = InfluxDBClient(host=conf.influxdb_host, port=conf.influxdb_port, username="admin", password="chords_ec_demo", database=conf.influxdb_database)
 
 def create_database(database_name):
     result = influx_client.create_database(database_name)
@@ -23,9 +23,17 @@ def create_measurement(payload):
     result = influx_client.write_points(payload)
     return result
 
-def query_measuremnts(query):
+def query_measuremnts(query_field_list):
     #logger.debug(query.get('q'))
-    result = influx_client.query(query.get('q')) #"SELECT \"value\" FROM \"cpu_load_short\" WHERE \"region\"='us-west'")
+    base_query = "SELECT \"value\" FROM \"tsdata\" WHERE "
+    query_list=[];
+    for itm in query_field_list:
+        fields = json.loads(itm)
+        for k in fields:
+            query_list.append("\""+k+"\"='"+str(fields[k])+"' ")
+    query_where = ' AND '.join(query_list)
+    logger.debug(base_query+query_where)
+    result = influx_client.query(base_query+query_where)
     return result.raw
 
 def list_measurements(instrument_id):
