b8df2346621aa4b8b6f493f698ca6ea812670088
ajamthetacc <34491583+ajamthetacc@users.noreply.github.com>
2020-10-20 12:03:26 -0500

    Update auth.py
    Added tenants=tenants on client generation

--- service/auth.py
+++ service/auth.py
@@ -69,7 +69,7 @@ def authentication():
     # this role is stored in the security kernel
 ROLE = 'streams_user'
 # this is the Tapis client that tenants will use for interacting with other services, such as the security kernel.
-t = auth.get_service_tapis_client(tenant_id='master')
+t = auth.get_service_tapis_client(tenant_id='master', tenants=tenants)
 t.x_username = conf.streams_user
 
 def authorization(skip_sk):
