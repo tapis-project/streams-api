a68cff2fcaef09d56c87d575f3ad97e99779f061
Smruti Padhy <spadhy@tacc.utexas.edu>
2020-11-30 16:39:35 -0600

    Fixed lambda expr list

--- service/parse_condition_expr.py
+++ service/parse_condition_expr.py
@@ -105,14 +105,15 @@ def get_chords_id_for_variable(key):
     logger.debug(result)
     if len(result) > 0:
         logger.debug(" chords instrument_ id: " + str(result[0]['chords_inst_id']))
-        inst_chords_id[cond_key[0]] = result[0]['chords_inst_id']
+        #inst_chords_id[cond_key[0]] = result[0]['chords_inst_id']
+        #inst_chords_id[cond_key[0]] = result[0]['instrument_id']
 
         # fetch chords id for the variable
         result_var, message = meta.get_variable(result[0]['project_id'], result[0]['site_id'],
                                                 result[0]['instrument_id'], cond_key[1])
         logger.debug("variable chords id : " + str(result_var['chords_id']))
         inst_var_chords_ids[key] = result_var['chords_id']
-        return result_var['chords_id']
+        return result[0]['chords_inst_id'],result_var['chords_id']
 
 def get_chords_id(key):
     keys_value={"1bclocal.templocal1": 1,
@@ -184,6 +185,7 @@ def convert_condition_list_to_vars( lambda_expr, lambda_expr_list, channel_id):
     vars["measurement"] = {"type":"string","value":"tsdata"}
 
     for i in range(len(lambda_expr_list)):
+        logger.debug("i: "+ str(i)+ ",  cond1: "+ str(lambda_expr_list[i][0])+ ",  expr: "+lambda_expr_list[i][2])
         vars['crit'+str(lambda_expr_list[i][0])] = {}
         vars['crit'+str(lambda_expr_list[i][0])]['type'] = "lambda"
         #  value is of the form : "value":"(\"var\" == '1') AND (\"value\" > 91.0)"
@@ -193,9 +195,9 @@ def convert_condition_list_to_vars( lambda_expr, lambda_expr_list, channel_id):
         #vars['th'+ str(lambda_expr_list[i][0])] = {}
         #vars['th' + str(lambda_expr_list[i][0])]['type'] = "float"
         #vars['th' + str(lambda_expr_list[i][0])]['value'] = lambda_expr_list[i][3]
-    vars['crit3'] = {}
-    vars['crit3']['type'] = "lambda"
-    vars['crit3']['value'] = lambda_expr
+    vars['crit'+str(lambda_expr_list[i][0]+1)] = {}
+    vars['crit'+str(lambda_expr_list[i][0]+1)]['type'] = "lambda"
+    vars['crit'+str(lambda_expr_list[i][0]+1)]['value'] = lambda_expr
     return vars
 
 #vars= {}
@@ -211,10 +213,9 @@ def parse_expr_list(exp_list, lambda_expr, count, lambda_expr_list, expr_list_ke
         #print(exp_list['op'], end=' ')
         #print(exp_list['val'], end=' ')
         #chords_id = get_chords_id(exp_list['key'])
-        chords_id = get_chords_id_for_variable(exp_list['key'])
-        inter = "(\"var\" == '" + str(chords_id) + "') AND (\"value\"" + exp_list['operator'] + str(
-            exp_list['val']) + ")"
-        lambda_expr_list.append((count,chords_id, inter, exp_list['val']))
+        chords_inst_id,chords_var_id = get_chords_id_for_variable(exp_list['key'])
+        inter = "(\"var\" == '" + str(chords_var_id) + "') AND (\"inst\" == '" + str(chords_inst_id) + "')"
+        lambda_expr_list.append((count, chords_var_id, inter, exp_list['val']))
         # with key
         expr_list_keys.append((count,exp_list['key']))
         count = count + 1
@@ -243,6 +244,7 @@ def parse_expr_list(exp_list, lambda_expr, count, lambda_expr_list, expr_list_ke
                 lambda_expr = lambda_expr + ')'
                 print(')', end=' ')
                 print(operator, end=' ')
+                lambda_expr = lambda_expr + ' ' + operator
             print('(', end=' ')
             lambda_expr = lambda_expr + '('
             lambda_expr, lambda_expr_list, count, expr_list_keys = parse_expr_list(exp_list[len_exp_list-1],lambda_expr,count,lambda_expr_list, expr_list_keys)
