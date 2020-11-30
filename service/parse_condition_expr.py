8b392f9552b0d761a333d15601c27f9f3bbfc5f6
Smruti Padhy <spadhy@tacc.utexas.edu>
2020-11-30 17:16:04 -0600

    commented code

--- service/parse_condition_expr.py
+++ service/parse_condition_expr.py
@@ -17,12 +17,11 @@ import meta
 t = auth.t
 
 
-global logical_op
-logical_op = [] # a stack of operators
-global state
-state = ''
+#global logical_op
+##global state
+#state = ''
 
-#----------- Example of condn expression ----------------------------------------------
+#----------- Example of condn_list expression ----------------------------------------------
 #cond_expr = ["AND",{"key":"1bclocal.templocal1", "op":">", "val":91.0},
 #                       ["OR",{"key":"1bclocal.templocal2", "op":">", "val":200.0},
 #                        {"key":"1bclocal.templocal3", "op":"<", "val":100.0}
@@ -30,67 +29,58 @@ state = ''
 #             ]
 #---------------------------------------------------------------------------------------
 
-#c=json.loads(json.dumps(cond_expr))
-
-#exp_list = c
-
-global lambda_expr
-
-lambda_expr=''
-
-def expr_to_lambda(exp_list):
-    global state
-
-    global lambda_expr
-    for i in range(len(exp_list)):
-        #print("i = " + str(i))
-        logical_op_in_exp_list0 = exp_list[0]
-        if i==0: # --When i is 0, look into the state variable to decide if you would want to append the logical operator
-                  # --( print
-            if logical_op_in_exp_list0 == "NOT":
-                #print("NOT", end=' ')
-                #print('(', end=' ')
-                lambda_expr = lambda_expr + 'NOT' + '('
-                state = "NOT"
-            else:
-                logical_op.append((logical_op_in_exp_list0,len(exp_list)-2))
-                if state != logical_op_in_exp_list0 and state !='' and state !='NOT':
-                    #print('(', end=' ')
-                    lambda_expr = lambda_expr + '('
-                state = logical_op_in_exp_list0
-            continue
-        elif isinstance(exp_list[i], dict):
-            #---print('trying to parse' + str(exp_list[i]))
-            #print(exp_list[i]['key'],end = ' ')
-            #print(exp_list[i]['op'],end = ' ')
-            #print(exp_list[i]['val'],end = ' ')
-
-            lambda_expr = lambda_expr + exp_list[i]['key'] + ' ' + exp_list[i]['op']+' ' + str(exp_list[i]['val']) + ' '
-            if i == (len(exp_list) - 1):
-                #print(')', end=' ')
-                lambda_expr = lambda_expr + ')'
-
-            if len(logical_op) != 0 and state != 'NOT':
-                op,opn = logical_op.pop()
-                if op!='NOT':
-                    #print(op,end = ' ')
-                    lambda_expr = lambda_expr + op + ' '
-                opn = opn - 1
-                state = op
-                if opn > 0:
-                    logical_op.append((op,opn))
-
-
-        else:
-            #print('trying to parse' + str(exp_list[i]))
-            expr_to_lambda(exp_list[i])
-
-
-#expr_to_lambda(exp_list)
-#print('\n')
-#print(lambda_expr)
+#global lambda_expr
 
 #lambda_expr=''
+
+# def expr_to_lambda(exp_list):
+#     global state
+#
+#     global lambda_expr
+#     for i in range(len(exp_list)):
+#         #print("i = " + str(i))
+#         logical_op_in_exp_list0 = exp_list[0]
+#         if i==0: # --When i is 0, look into the state variable to decide if you would want to append the logical operator
+#                   # --( print
+#             if logical_op_in_exp_list0 == "NOT":
+#                 #print("NOT", end=' ')
+#                 #print('(', end=' ')
+#                 lambda_expr = lambda_expr + 'NOT' + '('
+#                 state = "NOT"
+#             else:
+#                 logical_op.append((logical_op_in_exp_list0,len(exp_list)-2))
+#                 if state != logical_op_in_exp_list0 and state !='' and state !='NOT':
+#                     #print('(', end=' ')
+#                     lambda_expr = lambda_expr + '('
+#                 state = logical_op_in_exp_list0
+#             continue
+#         elif isinstance(exp_list[i], dict):
+#             #---print('trying to parse' + str(exp_list[i]))
+#             #print(exp_list[i]['key'],end = ' ')
+#             #print(exp_list[i]['op'],end = ' ')
+#             #print(exp_list[i]['val'],end = ' ')
+#
+#             lambda_expr = lambda_expr + exp_list[i]['key'] + ' ' + exp_list[i]['op']+' ' + str(exp_list[i]['val']) + ' '
+#             if i == (len(exp_list) - 1):
+#                 #print(')', end=' ')
+#                 lambda_expr = lambda_expr + ')'
+#
+#             if len(logical_op) != 0 and state != 'NOT':
+#                 op,opn = logical_op.pop()
+#                 if op!='NOT':
+#                     #print(op,end = ' ')
+#                     lambda_expr = lambda_expr + op + ' '
+#                 opn = opn - 1
+#                 state = op
+#                 if opn > 0:
+#                     logical_op.append((op,opn))
+#
+#
+#         else:
+#             #print('trying to parse' + str(exp_list[i]))
+#             expr_to_lambda(exp_list[i])
+
+
 
 def get_chords_id_for_variable(key):
     inst_chords_id = {}
@@ -115,68 +105,61 @@ def get_chords_id_for_variable(key):
         inst_var_chords_ids[key] = result_var['chords_id']
         return result[0]['chords_inst_id'],result_var['chords_id']
 
-def get_chords_id(key):
-    keys_value={"1bclocal.templocal1": 1,
-                "1bclocal.templocal2": 2,
-                "1bclocal.templocal3": 3
-                }
-    return keys_value[key]
+# def get_chords_id(key):
+#     keys_value={"1bclocal.templocal1": 1,
+#                 "1bclocal.templocal2": 2,
+#                 "1bclocal.templocal3": 3
+#                 }
+#     return keys_value[key]
 
-def get_all_crit_vars(exp_list,state,logical_op, lambda_expr,count,lambda_expr_list):
+# def get_all_crit_vars(exp_list,state,logical_op, lambda_expr,count,lambda_expr_list):
+#
+#     for i in range(len(exp_list)):
+#         logical_op_in_exp_list0 = exp_list[0]
+#         if i==0: # --When i is 0, look into the state variable to decide if you would want to append the logical operator
+#             if logical_op_in_exp_list0 == "NOT":
+#                 lambda_expr = lambda_expr + 'NOT' + '('
+#                 state = "NOT"
+#             else:
+#                 logical_op.append((logical_op_in_exp_list0,len(exp_list)-2))
+#                 if state != logical_op_in_exp_list0 and state !='' and state !='NOT':
+#                     lambda_expr = lambda_expr + '('
+#                 state = logical_op_in_exp_list0
+#             continue
+#         elif isinstance(exp_list[i], dict):
+#             #lambda_expr = lambda_expr + 'var'+str(count)+'.value' + ' ' + exp_list[i]['op'] + ' ' + 'th'+str(count) + ' '
+#             #lambda_expr = lambda_expr + 'var' + str(count) + '.value' + ' ' + exp_list[i]['operator'] + ' ' + str(exp_list[i]['val']) + ' '
+#             #lambda_expr = lambda_expr + "(\""+'var' + str(count) + '.value' + "\")"+ ' ' + exp_list[i]['operator'] + ' ' + str(
+#              #   exp_list[i]['val']) + ' '
+#             lambda_expr = lambda_expr + "\"" + 'var' + str(count) + '.value' + "\"" + ' ' + exp_list[i][
+#                 'operator'] + ' ' + str(
+#                 exp_list[i]['val']) + ' '
+#             #chords_id = get_chords_id(exp_list[i]['key'])
+#             logger.debug("key: " + exp_list[i]['key'])
+#             chords_id = get_chords_id_for_variable(exp_list[i]['key'])
+#             #inter =  "(\"var\" == '"+ str(chords_id)+"') AND (\"value\""+ exp_list[i]['operator'] +str(exp_list[i]['val'])+")"
+#             inter = "(\"var\" == '" + str(
+#         chords_id) + "') AND (\"value\"" + \
+#                             exp_list[i]['operator'] + str(exp_list[i]['val']) + ")"
+#             lambda_expr_list.append((count,chords_id,inter,exp_list[i]['val']))
+#             #lambda_expr = lambda_expr + exp_list[i]['key'] + ' ' + exp_list[i]['op']+' ' + str(exp_list[i]['val']) + ' '
+#
+#             count = count + 1
+#             if i == (len(exp_list) - 1):
+#                 lambda_expr = lambda_expr + ')'
+#
+#             if len(logical_op) != 0 and state != 'NOT':
+#                 op,opn = logical_op.pop()
+#                 if op!='NOT':
+#                    lambda_expr = lambda_expr + op + ' '
+#                 opn = opn - 1
+#                 state = op
+#                 if opn > 0:
+#                     logical_op.append((op,opn))
+#         else:
+#             lambda_expr, lambda_expr_list = get_all_crit_vars(exp_list[i],state,logical_op,lambda_expr,count,lambda_expr_list)
+#     return lambda_expr,lambda_expr_list
 
-    for i in range(len(exp_list)):
-        logical_op_in_exp_list0 = exp_list[0]
-        if i==0: # --When i is 0, look into the state variable to decide if you would want to append the logical operator
-            if logical_op_in_exp_list0 == "NOT":
-                lambda_expr = lambda_expr + 'NOT' + '('
-                state = "NOT"
-            else:
-                logical_op.append((logical_op_in_exp_list0,len(exp_list)-2))
-                if state != logical_op_in_exp_list0 and state !='' and state !='NOT':
-                    lambda_expr = lambda_expr + '('
-                state = logical_op_in_exp_list0
-            continue
-        elif isinstance(exp_list[i], dict):
-            #lambda_expr = lambda_expr + 'var'+str(count)+'.value' + ' ' + exp_list[i]['op'] + ' ' + 'th'+str(count) + ' '
-            #lambda_expr = lambda_expr + 'var' + str(count) + '.value' + ' ' + exp_list[i]['operator'] + ' ' + str(exp_list[i]['val']) + ' '
-            #lambda_expr = lambda_expr + "(\""+'var' + str(count) + '.value' + "\")"+ ' ' + exp_list[i]['operator'] + ' ' + str(
-             #   exp_list[i]['val']) + ' '
-            lambda_expr = lambda_expr + "\"" + 'var' + str(count) + '.value' + "\"" + ' ' + exp_list[i][
-                'operator'] + ' ' + str(
-                exp_list[i]['val']) + ' '
-            #chords_id = get_chords_id(exp_list[i]['key'])
-            logger.debug("key: " + exp_list[i]['key'])
-            chords_id = get_chords_id_for_variable(exp_list[i]['key'])
-            #inter =  "(\"var\" == '"+ str(chords_id)+"') AND (\"value\""+ exp_list[i]['operator'] +str(exp_list[i]['val'])+")"
-            inter = "(\"var\" == '" + str(
-        chords_id) + "') AND (\"value\"" + \
-                            exp_list[i]['operator'] + str(exp_list[i]['val']) + ")"
-            lambda_expr_list.append((count,chords_id,inter,exp_list[i]['val']))
-            #lambda_expr = lambda_expr + exp_list[i]['key'] + ' ' + exp_list[i]['op']+' ' + str(exp_list[i]['val']) + ' '
-
-            count = count + 1
-            if i == (len(exp_list) - 1):
-                lambda_expr = lambda_expr + ')'
-
-            if len(logical_op) != 0 and state != 'NOT':
-                op,opn = logical_op.pop()
-                if op!='NOT':
-                   lambda_expr = lambda_expr + op + ' '
-                opn = opn - 1
-                state = op
-                if opn > 0:
-                    logical_op.append((op,opn))
-        else:
-            lambda_expr, lambda_expr_list = get_all_crit_vars(exp_list[i],state,logical_op,lambda_expr,count,lambda_expr_list)
-    return lambda_expr,lambda_expr_list
-
-
-#lambda_expr,lambda_expr_list=get_all_crit_vars(exp_list,'',[],'',1,[])
-#print('\n')
-#print(lambda_expr)
-#print(lambda_expr_list)
-
-##### -------
 
 def convert_condition_list_to_vars( lambda_expr, lambda_expr_list, channel_id):
     #logger.debug("CONVERTING condition lisr to vars ...")
@@ -200,9 +183,7 @@ def convert_condition_list_to_vars( lambda_expr, lambda_expr_list, channel_id):
     vars['crit'+str(lambda_expr_list[i][0]+1)]['value'] = lambda_expr
     return vars
 
-#vars= {}
-#vars = convert_condition_list_to_vars( lambda_expr, lambda_expr_list, "test_channel")
-#print(vars)
+
 
 def parse_expr_list(exp_list, lambda_expr, count, lambda_expr_list, expr_list_keys):
     if isinstance(exp_list, dict):
