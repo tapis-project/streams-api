da16379ee44c2342930d36db64943ae3c5502d1b
Sean Cleveland <sean.b.cleveland@gmail.com>
2022-03-03 09:33:45 -0700

    fix abaco message passing

--- service/alerts.py
+++ service/alerts.py
@@ -184,7 +184,6 @@ def create_channel(req_body):
 
     #if req_body['triggers_with_actions'][0]['action']["method"] == "ACTOR":
     logger.debug("In Alert - before create  CHECK")
-    
     alert_url = conf.tenant[g.tenant_id]['tapis_base_url'] +'/v3/streams/alerts?tenant='+g.tenant_id
     notification_endpoint = checks.create_notification_endpoint_http(endpoint_name=channel_id+'_endpoint', 
                                                                      notification_url=alert_url)
