5a0cec35115ee91b530cd6afcd284b79a65a3924
Jeff Wong <42687471+jeff-yc-wong@users.noreply.github.com>
2023-08-22 14:58:23 -1000

    added other resource_types and stripped meta data from result

--- service/search.py
+++ service/search.py
@@ -1,6 +1,7 @@
 import requests
 import json
 import datetime
+import re
 from flask import g, Flask
 from tapisservice.config import conf
 app = Flask(__name__)
@@ -18,24 +19,37 @@ from service import meta
 t = auth.t
 
 
-def args_parse(request):
+def args_parse(request, resource_type):
     logger.info(f'Inside args_parse')
     query = request.args.to_dict(flat=False)
     logger.info(f'Searching for: {query}')
-    operators= ['neq', 'eq', 'nin','in']
-    project_params = ['project_name', 'project_id', 'owner', 'pi', 'funding_resource', 'project_url', 'active', 'created_at', 'last_updated']
+    operators = ['neq', 'eq', 'lte', 'lt', 'gte', 'gt', 'nin', 'in']
+    params = {
+        'project': ['project_name', 'project_id', 'owner', 'pi', 'funding_resource', 'project_url', 'active', 'created_at', 'last_updated'],
+        'site': ['site_name', 'site_id', 'owner', 'project_id', 'longitude', 'latitude', 'elevation', 'created_at', 'last_updated', 'metadata'],
+        'instrument': ['inst_name', 'inst_id', 'inst_description', 'owner', 'site_id', 'project_id', 'created_at', 'last_updated', 'metadata'],
+        'variable': ['var_name', 'var_id', 'owner', 'inst_id', 'shortname', 'unit', 'unit_abbrev', 'measured_property', 'created_at', 'last_updated', 'metadata']}
     # [(['project_id', 'in'], ['test']), (['owner', 'neq'], ['test1'])]
     search_list = [(k.split('.'), v) for k, v in query.items()]
     logger.info(search_list)
     logger.info(len(search_list))
     constructed_query = []
+    project_id_provided = -1
     # validation of search parameters
     for i in range(len(search_list)):
-        if(search_list[i][0][0] not in project_params):
+        if(search_list[i][0][0] == 'project_id'):
+            project_id_provided = i
+        if(search_list[i][0][0] not in params[resource_type]):
                 raise errors.ResourceError(msg=f'Invalid search parameter: {search_list[i][0][0]}.')
         if(search_list[i][0][1] not in operators):
                 raise errors.ResourceError(msg=f'Invalid operator: {search_list[i][0][1]}.')
         constructed_query.append([search_list[i][0][0],search_list[i][0][1],str(search_list[i][1]).strip('[,\',]')])
+    if(resource_type in ['site', 'instrument', 'variable'] and project_id_provided == -1):
+        raise errors.ResourceError(
+            msg=f'project_id is required when searching for site, instrument and variable.' )
+    if(resource_type in ['site', 'instrument', 'variable'] and search_list[project_id_provided][0][1] != 'eq'):
+        raise errors.ResourceError(
+            msg=f'project_id must be paired with the eq operator when searching for site, instrument and variable.')
     #logger.info(f' query constructed ')
     return constructed_query
 
@@ -72,6 +86,17 @@ def build_mongo_search_query(mongo_find, param, operator,search_value):
         else:
             mongo_find = mongo_find + '"' + param + \
             '":' + '{$nin:[\'' + search_on + '\']}}' 
+    elif(operator == "gt"):
+        try:
+            float(search_on)
+        except Exception as e:
+            try:
+                datetime.strptime(search_on, "%Y-%m-%d")
+            except Exception as e:
+                raise errors.ResourceError(msg="Error: Not a number or incorrect time string format (YYYY-mm-dd)")
+            
+        mongo_find = mongo_find + '"' + param + \
+        '":' + '{$gt:\'' + search_on + '\'}}'
     logger.info(mongo_find) 
     return mongo_find   
 
@@ -84,7 +109,7 @@ def build_mongo_search_query(mongo_find, param, operator,search_value):
 '''
 def project_search(request, skip, limit):
     logger.info(f'Inside project_search')
-    args_list = args_parse(request)
+    args_list = args_parse(request, "project")
     logger.info(args_list)
     #all_projects= meta.list_projects(100,0)
     #logger.info(all_projects)
@@ -93,7 +118,7 @@ def project_search(request, skip, limit):
         try:
             mongo_find = '{'
             logger.info(len(args_list))
-            for i in range (0,len(args_list)):
+            for i in range(0,len(args_list)):
                 param=args_list[i][0]
                 operator=args_list[i][1]
                 search_value=args_list[i][2]
@@ -129,8 +154,8 @@ def project_search(request, skip, limit):
                 sub_result = sub_result[0:int(limit)]
             logger.debug('before return')  
             logger.debug(sub_result)
-            return sub_result, message  
-         except Exception as e:
+            return sub_result, message
+          except Exception as e:
             logger.debug(e)
             raise errors.ResourceError(msg=str(e))
     else:
